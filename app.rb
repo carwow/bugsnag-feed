1a4ec0de21bef1f8f0f437fe89aeff3c44bc4ce5
Ken Alex Fassone <nexusventuri@gmail.com>
2016-05-12 22:18:21 +0000

    Increase ruby version

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/projects' do
 
   html = "<html><body>"
   html << "<dl>"
-  Project.all(token: auth_token).each do |project|
+  Project.all(token: params[:auth_token]).each do |project|
     project_url = url("/projects/#{project.id}?auth_token=#{auth_token}")
     html << "<dt>#{project.name}</dt><dd><a href='#{project_url}'>#{project_url}</a></dd>"
     { name: project.name, id: project.id}
@@ -21,10 +21,11 @@ get '/projects/:project_id' do
   auth_token = params[:auth_token]
 
   project = Project.find(params[:project_id], token: auth_token)
+  return status 500 if project.nil?
 
   %Q{<?xml version="1.0" encoding="UTF-8"?>
       <Projects>
-        <Project lastBuildStatus="#{project.status}" name="bugsnag - #{project_name}" activity="Sleeping" webUrl="#{project.html_url}">
+        <Project lastBuildStatus="#{project.status}" name="bugsnag - #{project.name}" activity="Sleeping" webUrl="#{project.html_url}">
         </Project>
       </Projects>}
 end
