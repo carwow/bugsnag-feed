85fcb7101c7d502f6a905aadf496da9c523392ef
David Santoro <soulnafein@gmail.com>
2016-05-18 10:30:05 +0000

    Removing individual project endpoint

--- app.rb
+++ app.rb
@@ -1,20 +1,5 @@
 require_relative 'projects.rb'
 
-get '/projects/:project_id' do
-  cache_control :public, max_age: 90
-  content_type 'text/xml'
-  auth_token = params[:auth_token]
-
-  project = Project.find(params[:project_id], token: auth_token)
-  return status 500 if project.nil? || project.status.nil?
-
-  %Q{<?xml version="1.0" encoding="UTF-8"?>
-      <Projects>
-        <Project lastBuildStatus="#{project.status}" name="bugsnag - #{project.name}" activity="Sleeping" webUrl="#{project.html_url}">
-        </Project>
-      </Projects>}
-end
-
 get '/projects' do
   cache_control :public, max_age: 90
   content_type 'text/xml'
