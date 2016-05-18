60c0e88d4b594a58db8b00ffc6da82b5191b7313
David Santoro <soulnafein@gmail.com>
2016-05-18 14:13:28 +0000

    Caching...

--- projects.rb
+++ projects.rb
@@ -3,19 +3,22 @@ class Project
   def_delegators :@project, :name, :id, :html_url
 
   def self.all(token:)
-    cache = Dalli::Client.new
     client = Bugsnag::Api::Client.new(auth_token: token)
-    
-    client.projects.map do |project|
-      key = "bugsnag-open-errors-#{project.id}"
-      number_of_open_errors = cache.get(key)
 
-      if number_of_open_errors.nil?
-        number_of_open_errors = client.errors(project.id, {status: 'open'}).count
-        cache.set(key, number_of_open_errors, 90)
+    project_key = 'bugsnag-projects'
+    projects = self.fetch(project_key, expires_in: 300) do
+      client.projects
+    end
+    
+    projects.map do |project|
+      key = "bugsnag-open-errors-#{project.id}"
+
+      open_errors_count = self.fetch(key) do
+        client.errors(project.id, {status: 'open'}).count
       end
 
-      self.new(project: project, open_errors_count: open_errors_count)
+      self.new(project: project, 
+               open_errors_count: open_errors_count)
     end
   end
 
@@ -28,5 +31,15 @@ class Project
     status = 'Success'
     status = 'Error' if @open_errors_count > 0
     status
+  end
+
+  def self.fetch(key, expires_in: 90)
+    cache = Dalli::Client.new
+    value = cache.get(key)
+    if value.nil?
+      value = yield
+      cache.set(key, value, expires_in)
+    end
+    value
   end
 end
