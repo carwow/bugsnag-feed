2d1bd2e3dbff5ed48c49fe48310329f5d6018087
David Santoro <soulnafein@gmail.com>
2016-05-18 09:55:20 +0000

    Aggregated project feed

--- app.rb
+++ app.rb
@@ -1,21 +1,5 @@
 require_relative 'projects.rb'
 
-get '/projects' do
-  cache_control :public, max_age: 60
-  content_type 'text/html'
-  auth_token = params[:auth_token]
-
-  html = "<html><body>"
-  html << "<dl>"
-  Project.all(token: auth_token).each do |project|
-    project_url = url("/projects/#{project.id}?auth_token=#{auth_token}")
-    html << "<dt>#{project.name}</dt><dd><a href='#{project_url}'>#{project_url}</a></dd>"
-    { name: project.name, id: project.id}
-  end
-  html << "</dl>"
-  html << "</body></html>"
-end
-
 get '/projects/:project_id' do
   cache_control :public, max_age: 90
   content_type 'text/xml'
@@ -29,4 +13,18 @@ get '/projects/:project_id' do
         <Project lastBuildStatus="#{project.status}" name="bugsnag - #{project.name}" activity="Sleeping" webUrl="#{project.html_url}">
         </Project>
       </Projects>}
+end
+
+get '/projects' do
+  cache_control :public, max_age: 90
+  content_type 'text/xml'
+  auth_token = params[:auth_token]
+
+  result = '<?xml version="1.0" encoding="UTF-8"?>'
+  result << '<Projects>'
+  Project.all(token: auth_token).each do |project|
+    result << %Q{<Project lastBuildStatus="#{project.status}" name="bugsnag - #{project.name}" activity="Sleeping" webUrl="#{project.html_url}"></Project>}
+  end
+  result << '</Projects>'
+  result
 end
